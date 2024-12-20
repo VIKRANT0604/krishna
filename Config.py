04d5012963cf1a8d6a20af2d4b681abe1cf41065
𝐋𝐄𝐆𝐄𝐍𝐃\𝐀𝐍𝐀𝐍𝐃 <viku20viku2000@gmail.com>
2024-12-20 18:46:20 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7921833487:AAGLwwe1xf3I-qjtIgCQSUO861fEgYlYM_8"
 STRING_SESSION = getenv("STRING_SESSION", "BQD7IGgAXdPk-f_fQqDkvebAVZE_lADEe10Ho6X5ri91jYNt8rPwCFS8OY1T1Iy1YvJmIwN_ITNcrwP09pCg7GSw1JaMwFb8Y-47FKs7kLQJP_8b6ZSCO9W3EYQ8fBOWU12xg42nlUTn3o8Cg1ajMDlj-Lr9Al3r7op-XnMh5LFsSglUWn-dHxo8SZHDM6OWlFeJt_BqYkKrZMT9Ev5_-Y7cAtFoQEqNShQUcIl8kUhCDvsp11c-uFQd9WfxcsOpdWhfeaIhggp3LJyBCw88OOZ2jlOkc4tUcTs64LZgiz3Tai4B3cQEkfCDYDXqC_Z96TSutbOkYoCZPuYUZeYC_GMMIuoPNAAAAAHR7uM4AA")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Yash_607:Yash_607@cluster0.r3s9sbo.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002043570167"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002078667802"))
 START_IMAGE_URL = getenv("START_IMAGE_URL", "https://files.catbox.moe/oq630k.jpg")
 REPO_IMAGE_URL = getenv("REPO_IMAGE_URL", "https://files.catbox.moe/he6axy.jpg")
 STATS_IMAGE_URL = getenv("STATS_IMAGE_URL", "https://files.catbox.moe/fj0l57.jpg")
